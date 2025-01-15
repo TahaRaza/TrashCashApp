5ad4eb0f11f63797d4218868d082b25daef6f08d
TahaHRaza <72659667+TahaRaza@users.noreply.github.com>
2025-01-15 23:19:42 +0500

    Changed theme

--- app/styles/theme.js
+++ app/styles/theme.js
@@ -1,28 +1,28 @@
-
-
 export const colors = {
-    buttonGreen: '#008000',  // green for buttons
-    buttonDarkGreen: '#006400',     // Dark green for buttons when pressed
-    white: '#FFFFFF',
-    themeBG: '#7FFF00',    // Light green for background or other elements
+  buttonGreen: "#4CAF50", // green for buttons
+  buttonDarkGreen: "#66BB6A", // Dark green for buttons when pressed
+  alerts: "#FF7043", // to contrast against the green theme
+  themeBG: "#E8FFE9", // Light green for background or other elements
+  highlights: "FFC107", //to emphasize important features
+  white: "111111",
 };
 
 export const buttonStyles = {
-    button: {
-        height: 50,
-        width: 200,
-        backgroundColor: colors.buttonGreen,
-        marginVertical: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 10,
-    },
-    buttonPressed: {
-        backgroundColor: colors.buttonDarkGreen,
-    },
-    buttonText: {
-        color: colors.white,
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
+  button: {
+    height: 50,
+    width: 200,
+    backgroundColor: colors.buttonGreen,
+    marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  buttonPressed: {
+    backgroundColor: colors.buttonDarkGreen,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 };
